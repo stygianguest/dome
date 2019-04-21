8422e1f436950b7d707ed42a9f2fe2f72cf53a84
Gideon Smeding <gideon@smdng.nl>
2019-04-21 21:01:55 +0200

    Use quaternions for the dotgui positions

--- src/DotGui.js
+++ src/DotGui.js
@@ -1,12 +1,6 @@
-import { mat4, mat3, vec2 } from 'gl-matrix';
+import { mat4, vec3, quat } from 'gl-matrix';
 import { Parameters } from './Parameters.js';
-import Renderer from './Renderer.js';
 import geometry from './geometry.js';
-import { AssertionError } from 'assert';
-
-function lerp(a, b, t) {
-    return a + t * (b-a);
-}
 
 function easeInOutCubic(t) {
     t *= 2.;
@@ -23,8 +17,15 @@ class DotGui {
             let phi = Math.random() * Math.PI - 0.5*Math.PI;
             let lambda = Math.random() * Math.PI * 2 - Math.PI;
 
-            this.locations[item] = { phi, lambda };
+            let q = quat.create();
+            quat.rotateX(q, q, lambda);
+            quat.rotateY(q, q, phi);
+            this.locations[item] = q;
         }
+
+        this.origin = quat.create();
+        this.pos = quat.create(); //TODO: rename to view
+        this.dest = quat.create();
 
         this.params = new Parameters("guiParams", (change) => {
             onUpdate();
@@ -35,23 +36,14 @@ class DotGui {
                 //TODO: updatng parameter values like this triggers superfluous recursive calls
                 //      it is essential we first change currentSelection
                 this.currentSelection = this.params.selection;
-                let pos = this.currentPosition();
-                this.params.origin.phi = pos.phi;
-                this.params.origin.lambda = pos.lambda;
-                this.params.dest.phi = this.locations[this.currentSelection].phi;
-                this.params.dest.lambda = this.locations[this.currentSelection].lambda;
+                quat.copy(this.origin, this.pos);
+                quat.copy(this.dest, this.locations[this.currentSelection]);
                 this.params.anim.t = 0.;
             }
         });
 
         this.currentSelection = menu[0];
         this.params.enum("selection", this.currentSelection, menu);
-        this.params.section("origin");
-        this.params.origin.float("phi", 0.0, 0.1, -2 * Math.PI, 2 * Math.PI);
-        this.params.origin.float("lambda", 0.0, 0.1, -2 * Math.PI, 2 * Math.PI);
-        this.params.section("dest");
-        this.params.dest.float("phi", -1., 0.1, -2 * Math.PI, 2 * Math.PI);
-        this.params.dest.float("lambda", -1., 0.1, -2 * Math.PI, 2 * Math.PI);
         this.params.section("anim");
         this.params.anim.float("t", 0., 0.0001, 0., 1.);
         this.params.anim.float("duration", 1.0, 0.1);
@@ -85,54 +77,17 @@ class DotGui {
             `);
     }
 
-    currentPosition() {
-        // this is an expensive function, early out for simple cases
-        if (this.params.anim.t == 0) {
-            return this.params.origin;
-        } else if ( this.params.anim.t == 1) {
-            return this.params.dest;
-        }
-
-        let t = easeInOutCubic(this.params.anim.t);
-
-        let o = this.params.origin;
-        let d = this.params.dest;
-
-        let delta = { phi: o.lambda - d.lambda, lambda: o.lambda - d.lambda };
-
-        // the haversine formula to compute the great circle distance
-        let dist = function() {
-            let sin_hphi = Math.sin(delta.phi/2);
-            let sin_hlambda = Math.sin(delta.lambda/2);
-            let a = sin_hphi*sin_hphi + Math.cos(o.phi) * Math.cos(d.phi) * sin_hlambda*sin_hlambda;
-            return 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        }();
-        
-        let sin_dist = Math.sin(dist);
-        let a = Math.sin((1-t)*dist) / sin_dist;
-        let b = Math.sin(t*dist) / sin_dist;
-        console.assert(sin_dist);
-        let x = a * Math.cos(o.phi) * Math.cos(o.lambda) + b * Math.cos(d.phi) * Math.cos(d.lambda);
-        let y = a * Math.cos(o.phi) * Math.sin(o.lambda) + b * Math.cos(d.phi) * Math.sin(d.lambda);
-        let z = a * Math.sin(o.phi) + b * Math.sin(d.phi);
-        let phi = Math.atan2(z, Math.sqrt(x*x + y*y));
-        let lambda = Math.atan2(y, x);
-
-        //console.log(phi, lambda);
-        // console.assert(!isNaN(phi));
-        // console.assert(!isNaN(lambda));
-
-
-        return { phi, lambda };
-    }
-
     update(dtime) {
         if (this.params.anim.t < 1) {
+            // we're still animating, update the current position
             this.params.anim.t += dtime / this.params.anim.duration;
             this.params.anim.t = Math.min(1., this.params.anim.t);
+            quat.slerp(this.pos, this.origin, this.dest, easeInOutCubic(this.params.anim.t));
         } else {
-            this.params.origin.phi = this.params.dest.phi;
-            this.params.origin.lambda = this.params.dest.lambda;
+            // animation is done
+            //FIXME: does it make sense to copy every time? can we skip this?
+            quat.copy(this.origin, this.dest);
+            quat.copy(this.pos, this.dest);
         }
     }
 
@@ -145,21 +100,22 @@ class DotGui {
             const fieldOfView = Math.atan2(1.0, distance) * 2.0;
             const zNear = distance;
             const zFar = 100.0;
-            let pos = this.currentPosition();
 
             let m = mat4.create();
             mat4.perspective(m, fieldOfView, aspect, zNear, zFar);
             mat4.translate(m, m, [0., 0., -distance]);
             mat4.rotate(m, m, Math.PI, [1, 0, 0]);
 
-            mat4.rotate(m, m, -pos.lambda, [0, 1, 0]);
-            mat4.rotate(m, m, -pos.phi, [1, 0, 0]);
-            //mat4.rotate(m, m, pos.phi, [1, 0, 0]);
-            //mat4.rotate(m, m, pos.phi, [1, 0, 0]);
-            //mat4.rotate(m, m, pos.lambda, [0, 1, 0]);
-            //mat4.rotate(m, m, 0.9, [0, 1, 0]);
-            //mat4.rotate(m, m, Math.PI*0.5, [1, 0, 0]);
-            this.uniforms.projectionMatrix = m;
+            { // rotate
+                let q = quat.create();
+                quat.copy(q, this.pos);
+                quat.invert(q, q);
+                let r = mat4.create();
+                mat4.fromQuat(r, q);
+                mat4.multiply(m, m, r);
+            }
+
+           this.uniforms.projectionMatrix = m;
         }
 
         for (let item in this.locations) {
@@ -167,10 +123,12 @@ class DotGui {
             // rotate the object
             let pos = this.locations[item];
             let m = mat4.create();
-            mat4.rotate(m, m, pos.phi, [1, 0, 0]);
-            mat4.rotate(m, m, pos.lambda, [0, 1, 0]);
-            //mat4.rotate(m, m, 0.3, [1, 0, 0]);
-            //mat4.rotate(m, m, 0.9, [0, 1, 0]);
+            { // rotate
+                let r = mat4.create();
+                mat4.fromQuat(r, pos);
+                mat4.multiply(m, m, r);
+            }
+
             mat4.translate(m, m, [0., 0., 1]);
             mat4.scale(m, m, [0.1, 0.1, 0.1]);
             this.uniforms.modelMatrix = m;
